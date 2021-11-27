457a20d35ecd18c33918fdb54ae413721f4a822d
GaoHuanYan <90028430+GaoHuanYan@users.noreply.github.com>
2021-11-27 15:21:40 +0800

    Add files via upload

---
+++ FileIO.h
@@ -0,0 +1,10 @@
+//FIleIO.h
+#ifndef FileIO_H
+#define FileIO_H
+
+void end(int length);
+void fileout(int ** &snake, int food [2], int length);
+void filein(int &length, char area[47][17], int ** &snake, int food[2]);
+
+
+# endif;
